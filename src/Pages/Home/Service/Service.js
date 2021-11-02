283c1e95f8a54f40116b974e518b3820d47f932d
masum691 <masumhu691@gmail.com>
2021-11-02 19:48:11 +0600

    added card image

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -5,32 +5,32 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
 
-const Service = ({service}) => {
-    const {name, desc, img} = service;
+const Service = ({ service }) => {
+    const { name, desc, img } = service;
     return (
         <Grid item xs={4} sm={4} md={4}>
             <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-            Service
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+                <CardMedia
+                    component="img"
+                    style={{width: '64px', height: '64px', margin: '0px auto'}}
+                    image={img}
+                />
+                <CardContent>
+                    <Typography variant="h5" component="div">
+                        Service
+                    </Typography>
+                    <Typography variant="body2">
+                        well meaning and kindly.
+                        <br />
+                        {'"a benevolent smile"'}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
         </Grid>
     );
 };

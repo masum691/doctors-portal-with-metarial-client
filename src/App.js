b327b9769b27c8b289d5c5305095ca86752769c1
masum691 <masumhu691@gmail.com>
2021-11-02 14:41:22 +0600

    setup router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
+import Footer from './Pages/Shared/Footer/Footer';
+import Navbar from './Pages/Shared/Navbar/Navbar';
 
 function App() {
   return (
     <div>
-      <h1>Checking site</h1>
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }

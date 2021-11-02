51495beea363708f0f3b063e3edfb5a725bc4a41
masum691 <masumhu691@gmail.com>
2021-11-02 19:13:02 +0600

    load data

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -4,6 +4,10 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { Container } from '@mui/material';
+import flouride from '../../../images/fluoride.png'
+import cavity from '../../../images/cavity.png'
+import whitening from '../../../images/whitening.png'
+import Service from '../Service/Service';
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -13,16 +17,32 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
 }));
 
-
+const services = [
+    {
+        name: 'Floride Treatment',
+        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit id, molestias dolorem expedita, cumque omnis, consequatur pariatur quia non qui ratione voluptatem!',
+        img: flouride
+    },
+    {
+        name: 'Cavity Treatment',
+        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit id, molestias dolorem expedita, cumque omnis, consequatur pariatur quia non qui ratione voluptatem!',
+        img: cavity
+    },
+    {
+        name: 'Whitening Treatment',
+        desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit id, molestias dolorem expedita, cumque omnis, consequatur pariatur quia non qui ratione voluptatem!',
+        img: whitening
+    }
+]
 
 const Services = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Container>
             <Grid container spacing={{ xs: 4, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                {Array.from(Array(6)).map((_, index) => (
+                {services.map((_, index) => (
                     <Grid item xs={4} sm={4} md={4} key={index}>
-                        <Item>xs=2</Item>
+                        <Service>xs=2</Service>
                     </Grid>
                 ))}
             </Grid>

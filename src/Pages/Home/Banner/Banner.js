d67c33ab9d10948d7f6fe0d8cf11f18964209897
masum691 <masumhu691@gmail.com>
2021-11-03 17:01:13 +0600

    banner

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import chair from '../../../images/chair.png'
+import bg from '../../../images/bg.png'
+import Grid from '@mui/material/Grid';
+import { Button, Container, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+
+const bannerBg = {
+    banner: `url(${bg})`,
+}
+const verticalCenter = {
+    display: 'flex',
+    alignItems: 'center',
+    height: 400
+}
+const Banner = () => {
+
+    return (
+        <Container style={bannerBg} sx={{ width: '100%' }}>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Grid item xs={12} md={6} style={verticalCenter}>
+                    <Box>
+                    <Typography variant="h3" sx={{mb: 2}}>
+                        Your new smile start here
+                    </Typography>
+                    <Typography variant="p" >
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam suscipit officia culpa nulla harum a, ut ipsam nam, error animi eveniet ratione.
+                    </Typography>
+                    <br />
+                    <Button sx={{mt: 2}} variant="outlined">Learn More</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} md={6} style={verticalCenter}>
+                    <img style={{ width: '400px' }} src={chair} alt="" />
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AppoinmentBanner from '../AppoinmentBanner/AppoinmentBanner';
+import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <Services></Services>
             <AppoinmentBanner></AppoinmentBanner>
         </div>

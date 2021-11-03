b1059a74c8c502233adf48c283f41da9dd3958ec
masum691 <masumhu691@gmail.com>
2021-11-03 16:10:03 +0600

    image popup and background

--- src/Pages/Home/AppoinmentBanner/AppoinmentBanner.js
+++ src/Pages/Home/AppoinmentBanner/AppoinmentBanner.js
@@ -4,27 +4,41 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import doctor from '../../../images/doctor.png'
 import appoinment from '../../../images/appointment-bg.png'
+import Button from '@mui/material/Button';
 
 const appoinmentBg = {
-    background: `url(${appoinment})`
+    background: `url(${appoinment})`,
+    marginTop: '120px',
+    backgroundColor: 'rgba(2, 11, 20, 0.663)',
+    backgroundBlendMode: 'darken'
 }
 
 const AppoinmentBanner = () => {
     return (
+
         <Box style={appoinmentBg} sx={{ width: '100%' }}>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={6}>
-          <img 
-           style={{height: '600px'}}
-           src={doctor} alt="" />
-        </Grid>
-        <Grid item xs={6}>
-            <Typography variant="h3">
-                Lorem ipsum dolor sit.
-            </Typography>
-        </Grid>
-      </Grid>
-    </Box>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Grid item xs={6}>
+                    <img
+                        style={{ height: '600px', marginTop: '-150px' }}
+                        src={doctor} alt="" />
+                </Grid>
+                <Grid item xs={12} md={6} sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
+                    <Box>
+                        <Typography variant="h5" style={{ color: '#0590C4' }}>
+                            Appoinment
+                        </Typography>
+                        <Typography variant="h4" sx={{my: 2}}>
+                            Make an Appoinment today
+                        </Typography>
+                        <Typography variant="p">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quo id debitis molestias recusandae blanditiis quae doloribus eligendi, aspernatur molestiae, ullam dolore.
+                        </Typography>
+                        <Button variant="contained" disableElevation sx={{mt:2}}>See More</Button>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
     );
 };
 

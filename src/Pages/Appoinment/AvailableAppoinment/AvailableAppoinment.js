c8768aa9f13cdef0dadd217cf043a2d54a3ccf24
masum691 <masumhu691@gmail.com>
2021-11-04 12:41:24 +0600

    booking component

--- src/Pages/Appoinment/AvailableAppoinment/AvailableAppoinment.js
+++ src/Pages/Appoinment/AvailableAppoinment/AvailableAppoinment.js
@@ -1,10 +1,58 @@
+import { Container, Grid } from '@mui/material';
 import React from 'react';
+import Booking from '../Booking/Booking';
 
 const AvailableAppoinment = ({date}) => {
+    const bookings = [
+        {
+            id: 1,
+            name: 'Lorem Ipsum',
+            time: '07.00 PM - 08.00 PM',
+            space: 12
+        },
+        {
+            id: 2,
+            name: 'Lorem Ipsum',
+            time: '05.00 PM - 08.00 PM',
+            space: 10
+        },
+        {
+            id: 3,
+            name: 'Lorem Ipsum',
+            time: '03.00 PM - 06.00 PM',
+            space: 15
+        },
+        {
+            id: 4,
+            name: 'Lorem Ipsum',
+            time: '11.00 PM - 12.00 AM',
+            space: 22
+        },
+        {
+            id: 5,
+            name: 'Lorem Ipsum',
+            time: '06.00 PM - 08.00 PM',
+            space: 20
+        },
+        {
+            id: 6,
+            name: 'Lorem Ipsum',
+            time: '07.00 PM - 08.00 PM',
+            space: 16
+        }
+    ]
     return (
-        <div>
+        <Container>
             <h1>Available date {date.toDateString()}</h1>
-        </div>
+            <Grid container spacing={2}>
+                {
+                    bookings.map(booking => <Booking
+                    key={booking.id}
+                    booking={booking}
+                    ></Booking>)
+                }
+            </Grid>
+        </Container>
     );
 };
 

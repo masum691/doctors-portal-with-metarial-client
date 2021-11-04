0aac87f261e880242b308bff4e34d253785ce374
masum691 <masumhu691@gmail.com>
2021-11-04 11:40:10 +0600

    calander

--- src/Pages/Appoinment/AppoinmentHeader/AppoinmentHeader.js
+++ src/Pages/Appoinment/AppoinmentHeader/AppoinmentHeader.js
@@ -1,10 +1,20 @@
+import { Container, Grid } from '@mui/material';
 import React from 'react';
+import chair from '../../../images/chair.png'
+import Calander from '../../Shared/Calander/Calander';
 
 const AppoinmentHeader = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                    <Calander></Calander>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <img style={{width: '100%'}} src={chair} alt="" />
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 

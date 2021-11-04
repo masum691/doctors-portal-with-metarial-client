e37f972f2490fbb416c0b8e01fc7260d7f255e65
masum691 <masumhu691@gmail.com>
2021-11-04 13:43:49 +0600

    modal and dynamic data

--- src/Pages/Appoinment/Booking/Booking.js
+++ src/Pages/Appoinment/Booking/Booking.js
@@ -1,24 +1,35 @@
 import { Button, Grid, Typography } from '@mui/material';
 import React from 'react';
 import Paper from '@mui/material/Paper';
+import BookingModal from '../BookingModal/BookingModal';
 
-const Booking = ({booking}) => {
-    const {name, time, space} = booking;
+const Booking = ({ booking }) => {
+    const { name, time, space } = booking;
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     return (
-        <Grid item xs={12} sm={6} md={4}>
-            <Paper elevation={3} sx={{p: 2 , textAlign: 'center'}}>
-                <Typography sx={{color: 'info.main'}} component="div" gutterBottom variant="h5">
-                    {name}
-                </Typography>
-                <Typography component="div" gutterBottom variant="h6">
-                    {time}
-                </Typography>
-                <Typography component="div" gutterBottom variant="p">
-                    {space} spaces available
-                </Typography>
-                <Button sx={{mt:2}} variant="contained">Book Appoinment</Button>
-            </Paper>
-        </Grid>
+        <>
+            <Grid item xs={12} sm={6} md={4}>
+                <Paper elevation={3} sx={{ p: 2, textAlign: 'center' }}>
+                    <Typography sx={{ color: 'info.main' }} component="div" gutterBottom variant="h5">
+                        {name}
+                    </Typography>
+                    <Typography component="div" gutterBottom variant="h6">
+                        {time}
+                    </Typography>
+                    <Typography component="div" gutterBottom variant="p">
+                        {space} spaces available
+                    </Typography>
+                    <Button onClick={handleOpen} sx={{ mt: 2 }} variant="contained">Book Appoinment</Button>
+                </Paper>
+            </Grid>
+            <BookingModal
+             booking={booking}
+             open={open}
+             handleClose={handleClose}
+            ></BookingModal>
+        </>
     );
 };
 

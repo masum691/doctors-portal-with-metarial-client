b6ee57c8a1d9de5f9f2f51d23528ece67069ef17
masum691 <masumhu691@gmail.com>
2021-11-04 11:24:00 +0600

    available appoinment

--- src/Pages/Appoinment/Appoinment/Appoinment.js
+++ src/Pages/Appoinment/Appoinment/Appoinment.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import AppoinmentHeader from '../AppoinmentHeader/AppoinmentHeader';
+import AvailableAppoinment from '../AvailableAppoinment/AvailableAppoinment';
 
 const Appoinment = () => {
     return (
         <div>
-            
+            <AppoinmentHeader></AppoinmentHeader>
+            <AvailableAppoinment></AvailableAppoinment>
         </div>
     );
 };
---
+++ src/Pages/Appoinment/AppoinmentHeader/AppoinmentHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppoinmentHeader = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AppoinmentHeader;
